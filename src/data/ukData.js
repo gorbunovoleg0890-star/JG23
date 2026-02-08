dae5b0144162516f4e470d984f4f1e65deadfefa
gorbunovoleg0890-star <gorbunov.oleg.0890@gmail.com>
2026-02-08 09:43:37 +0000

    feat: add restriction of freedom as additional punishment option

--- src/data/ukData.js
+++ src/data/ukData.js
@@ -470,6 +470,12 @@ export const punishmentTypes = [
     additional: false
   },
   {
+    id: 'restriction-of-freedom',
+    label: 'Ограничение свободы',
+    primary: true,
+    additional: true
+  },
+  {
     id: 'arrest',
     label: 'Арест',
     primary: true,
